c629b3cea8263eb65df81340fa308dd066a1bfdc
Tadej Borovšak <tadeboro@gmail.com>
2015-01-16 09:21:19 +0100

    Finish lab11

--- dn11_27142027.rkt
+++ dn11_27142027.rkt
@@ -52,3 +52,39 @@
 ((stevilo-nacinov-memo 3))
 ((stevilo-nacinov-memo 4))
 ((stevilo-nacinov-memo 5))
+
+
+(define (vector-assoc v vec)
+  (define (isci n)
+    (let ([trenutni (vector-ref vec n)])
+      (cond [(>= n (vector-length vec)) #f]
+            [(pair? trenutni)
+             (if (equal? (car trenutni) v)
+               trenutni
+               (isci (+ n 1)))])))
+  (isci 0))
+
+(define (cached-assoc sez n)
+  (let ([cache (make-vector n #t)]
+        [counter 0])
+    (define (assocb x)
+      (let ([odg (vector-assoc x cache)])
+        (if odg
+          odg
+          (let ([novi-odg (assoc x sez)])
+            ((vector-set! cache counter novi-odg)
+             (set! counter (remainder (+ counter 1) n))
+             novi-odg)))))
+    assocb))
+
+(define (odstej e1 e2) (sestej (e1 (negacija e2))))
+(define (sestej-ali-odstej pogoj e1 e2)
+  (ce-potem-sicer pogoj
+                  (sestej e1 e2)
+                  (odstej e1 e2)))
+(define (nobeden e1 e2)
+  (ce-potem-sicer e1
+                  (bool false)
+                  (ce-potem-sicer e2
+                                  (bool false)
+                                  (bool true))))
